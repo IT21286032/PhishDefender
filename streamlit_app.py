2e22c9cae6b253696cd71fe3ffeaae6bfecb092a
Kajeevan J <99233247+IT21286032@users.noreply.github.com>
2023-10-21 01:57:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,16 +75,20 @@ st.write('This ML-based app is developed for educational purposes. The objective
 
 with st.expander("PROJECT DETAILS:"):
     
-    st.subheader('Approach')
-    st.write('I used _supervised learning_ to classify phishing and legitimate websites. '
-             'I benefit from content-based approach and focus on html of the websites. '
-             'Also, I used scikit-learn for the ML models.'
-             )
-    st.write('For this educational project, '
-             'I created my own data set and defined features, some from the literature and some based on manual analysis. '
-             'I used requests library to collect data, BeautifulSoup module to parse and extract features. ')
-    st.write('The source code and data sets are available in the below Github link:')
-    st.write('_https://github.com/emre-kocyigit/phishing-website-detection-content-based_')
+    st.subheader('What is Phish Defender')
+    st.write("""
+## Welcome to the Phishing Website Detection Tool!
+This tool allows you to input a website URL to determine whether it might be a potential phishing site. 
+Our system uses advanced machine learning algorithms to analyze various features of the provided URL 
+and predict its legitimacy.
+
+Simply enter the URL in the input box below and click on 'Check!' to get the results.
+
+**Note**: Always exercise caution when browsing the internet and avoid entering personal information 
+on suspicious websites. This tool is meant to aid in detection and is not a definitive measure of a website's legitimacy.
+
+Enjoy your safe browsing!
+""")
 
     st.subheader('Data set')
     st.write('I used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
